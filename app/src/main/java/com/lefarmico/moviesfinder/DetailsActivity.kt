58dff8cc35c50d062048e47c267e0b04e7a06244
Artyom Zharnicovich <62179029+LeFarmico@users.noreply.github.com>
2020-12-03 12:57:44 +0300

    delete adapter folder

---
+++ app/src/main/java/com/lefarmico/moviesfinder/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.lefarmico.moviesfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lefarmico.moviesfinder.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
