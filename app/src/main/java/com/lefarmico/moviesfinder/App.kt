b02ccc4a5a68538d1994b74f0abb2f53c761337a
Artyom Zharnicovich <62179029+LeFarmico@users.noreply.github.com>
2021-06-09 20:28:52 +0200

    Rework SearchFragment.kt

--- app/src/main/java/com/lefarmico/moviesfinder/App.kt
+++ app/src/main/java/com/lefarmico/moviesfinder/App.kt
@@ -1,17 +1,13 @@
 package com.lefarmico.moviesfinder
 
 import android.app.Application
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
 import android.util.Log
 import com.lefarmico.moviesfinder.di.*
-import com.lefarmico.moviesfinder.notifications.NotificationConstants.CHANNEL_ID
 import com.lefarmico.remote_module.DaggerRemoteComponent
 
 class App : Application() {
 
-    private val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+//    private val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
 
     companion object {
         lateinit var appComponent: AppComponent
@@ -34,17 +30,17 @@ class App : Application() {
             .domainModule(DomainModule(this))
             .build()
 
-        createNotificationChannel()
+//        createNotificationChannel()
     }
 
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "WatchLaterChannel"
-            val descriptionText = "FilmsSearch notification Channel"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
-            mChannel.description = descriptionText
-            notificationManager.createNotificationChannel(mChannel)
-        }
-    }
+//    private fun createNotificationChannel() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            val name = "WatchLaterChannel"
+//            val descriptionText = "FilmsSearch notification Channel"
+//            val importance = NotificationManager.IMPORTANCE_DEFAULT
+//            val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
+//            mChannel.description = descriptionText
+//            notificationManager.createNotificationChannel(mChannel)
+//        }
+//    }
 }
