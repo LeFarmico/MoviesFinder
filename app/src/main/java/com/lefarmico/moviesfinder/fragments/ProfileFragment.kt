38e09cf05e480415db5d6180e9038eec3d928526
Artyom Zharnicovich <62179029+LeFarmico@users.noreply.github.com>
2021-06-14 20:12:09 +0200

    Fix movie details layout bug

--- app/src/main/java/com/lefarmico/moviesfinder/fragments/ProfileFragment.kt
+++ app/src/main/java/com/lefarmico/moviesfinder/fragments/ProfileFragment.kt
@@ -6,13 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.lefarmico.moviesfinder.databinding.FragmentProfileBinding
+import com.lefarmico.moviesfinder.viewModels.ProfileFragmentViewModel
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
 
+    val viewModel: ProfileFragmentViewModel by viewModels()
     private val TAG = this.javaClass.canonicalName
 
     override fun onCreateView(
