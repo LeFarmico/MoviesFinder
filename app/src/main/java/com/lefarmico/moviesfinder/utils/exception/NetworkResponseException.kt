a3edd1d0edf70b8e8e95de8b2ef4b2e8d81417be
Artyom Zharnicovich <62179029+LeFarmico@users.noreply.github.com>
2022-11-12 22:45:48 +0100

    feature: added NetworkResponseException.kt with code and message in constructors

---
+++ app/src/main/java/com/lefarmico/moviesfinder/utils/exception/NetworkResponseException.kt
@@ -0,0 +1,12 @@
+package com.lefarmico.moviesfinder.utils.exception
+
+class NetworkResponseException : Exception {
+    constructor() : super()
+    constructor(code: Int?, message: String) : super(getMessage(code, message))
+    constructor(code: Int?, message: String, cause: Throwable) : super(getMessage(code, message), cause)
+    constructor(cause: Throwable) : super(cause)
+
+    companion object {
+        private fun getMessage(code: Int?, message: String?) = "[CODE] $code, [MESSAGE] $message"
+    }
+}
